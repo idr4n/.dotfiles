2d019c8fe2d0415a008932456d3f3c9030bd6bdf
Ivan Duran <idurandiaz@gmail.com>
2022-08-04 19:56:02 +0300

    Kitty changes

--- scripts/change_dark_mode.py
+++ scripts/change_dark_mode.py
@@ -1,14 +1,18 @@
 import os
 import darkdetect
 
-light_color_kitty = "Zenbones Light Custom"
+# light_color_kitty = "Zenbones Light Custom"
+light_color_kitty = "nordfox"
 # dark_color_kitty = "Tokyo Night Custom"
 # dark_color_kitty = "Nord Custom"
-dark_color_kitty = "Zenbones Dark Custom"
+# dark_color_kitty = "Zenbones Dark Custom"
+dark_color_kitty = "duskfox"
 # dark_color_kitty = "Rasmus"
-light_color_alacritty = "zenbones_light"
+# light_color_alacritty = "zenbones_light"
+light_color_alacritty = "nordfox"
 # dark_color_alacritty = "tokyo_night"
-dark_color_alacritty = "zenbones_dark"
+# dark_color_alacritty = "zenbones_dark"
+dark_color_alacritty = "duskfox"
 # dark_color_alacritty = "rasmus"
 
 if os.environ.get("HOME") == "/Users/iduran":
